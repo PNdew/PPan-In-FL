0dfa5cf8b0cbb957b985232cd62c5e9d333b2888
PNdew <phuc19082k4@gmail.com>
2025-03-25 15:04:06 +0700

    Local_Differential_Privacy

--- Local_Differential_Privacy/config.py
+++ Local_Differential_Privacy/config.py
@@ -6,9 +6,9 @@ import torch
 NUM_CLIENTS = 100
 K_CLIENTS = 10
 BATCH_SIZE = 16
-NUM_ROUNDS = 3
+NUM_ROUNDS = 200
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.1
 
 # Privacy parameters
 INITIAL_EPSILON = 2.0
