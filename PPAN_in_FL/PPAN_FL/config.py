251263285b7db97abc44b0ef70f92dbb61f8ac45
PNdew <phuc19082k4@gmail.com>
2025-03-25 15:01:53 +0700

    PPAN_in_FL

--- PPAN_in_FL/PPAN_FL/config.py
+++ PPAN_in_FL/PPAN_FL/config.py
@@ -5,9 +5,9 @@ import os
 NUM_CLIENTS = 100
 BATCH_SIZE = 16
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-LEARNING_RATE = 0.001
-PRIVACY_WEIGHT = 0.001
-NUM_ROUNDS = 1000
+LEARNING_RATE = 0.1
+PRIVACY_WEIGHT = 0.01
+NUM_ROUNDS = 200
 NOISE_SCALE = 0.01
 
 # Results directory
